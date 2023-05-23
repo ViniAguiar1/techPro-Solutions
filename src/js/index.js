794d5fa326ca46cf1b4890498f56d8756066995b
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-05-23 20:34:53 -0300

    Segunda atualização

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,24 @@
 let setupBrown = document.getElementById("setupBrown")
 let setupWhite = document.getElementById("setupWhite")
+let setup2 = document.getElementById("setup2")
+let setup4 = document.getElementById("setup4")
 
 function mostrarSetupDark(){
     setupBrown.style = "display:none"
     setupWhite.style = "display:flex"
+}
+
+function mostrarSetupBrown() {
+    setupBrown.style = "display:flex"
+    setupWhite.style = "display:none"
+}
+
+function mostrarSetup4(){
+    setup2.style = "display:none"
+    setup4.style = "display:flex"
+}
+
+function mostrarSetup2(){
+    setup2.style = "display:flex"
+    setup4.style = "display:none"
 }
