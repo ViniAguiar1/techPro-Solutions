39757308069019360ce3821b48b79d102b90e3d0
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-05-23 14:17:32 -0300

    Att

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+let setupBrown = document.getElementById("setupBrown")
+let setupWhite = document.getElementById("setupWhite")
+
+function mostrarSetupDark(){
+    setupBrown.style = "display:none"
+    setupWhite.style = "display:flex"
+}
